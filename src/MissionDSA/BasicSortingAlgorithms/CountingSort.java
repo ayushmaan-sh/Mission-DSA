8f031b9ab8e2c56c109992d86a826d6881254752
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-10-22 17:17:34 +0530

    Counting sort

---
+++ src/MissionDSA/BasicSortingAlgorithms/CountingSort.java
@@ -0,0 +1,12 @@
+package MissionDSA.BasicSortingAlgorithms;
+
+public class CountingSort {
+
+    public static void countSort(int arr[]){
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,1,6,2,6,4,6};
+    }
+}

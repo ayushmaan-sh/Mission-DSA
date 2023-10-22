694e093c09550a894127622d1a16d0c759ded037
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-10-22 15:43:08 +0530

    Inbuilt sort

--- src/MissionDSA/BasicSortingAlgorithms/InbuiltSort.java
+++ src/MissionDSA/BasicSortingAlgorithms/InbuiltSort.java
@@ -1,4 +1,18 @@
 package MissionDSA.BasicSortingAlgorithms;
-
+//In this, we already have a in built function in a java package
+import java.util.Arrays;
 public class InbuiltSort {
+
+    // Function for printing array
+    public static void printArray(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,4,1,5,2};
+        Arrays.sort(arr);
+        printArray(arr);
+    }
 }

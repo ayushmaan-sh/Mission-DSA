d6c631fdf91f8363d22751367f094039676bf162
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-10-25 06:46:48 +0530

    Inheritance

--- src/MissionDSA/ObjectOrientedProgramming/Inheritance.java
+++ src/MissionDSA/ObjectOrientedProgramming/Inheritance.java
@@ -5,6 +5,24 @@ package MissionDSA.ObjectOrientedProgramming;
 public class Inheritance {
 
     public static void main(String[] args) {
+        cricketer c = new cricketer();
+        c.bowl();
+    }
+}
 
+class Athlete {
+    float height;
+    void run(){
+        System.out.println("Player can run");
+    }
+    void jump(){
+        System.out.println("Player can jump");
+    }
+}
+//Class cricketer inherit class Athlete
+class cricketer extends Athlete{
+    float bowling_Speed;
+    void bowl(){
+        System.out.println("Player can bowl in cricket...");
     }
 }

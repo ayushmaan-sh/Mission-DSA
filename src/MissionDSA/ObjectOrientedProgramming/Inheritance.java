0421e15a28eac6faf9fa552d34709fa95db04598
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-10-25 06:29:38 +0530

    Inheritance

---
+++ src/MissionDSA/ObjectOrientedProgramming/Inheritance.java
@@ -0,0 +1,10 @@
+package MissionDSA.ObjectOrientedProgramming;
+
+//Inheritance is when properties and methods of base class are passed on to a derived class.
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+    }
+}

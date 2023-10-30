88fe0e5f1c94cacdc91c325d10787a6e36bfe61b
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-10-30 05:51:54 +0530

    Print in increasing order

---
+++ src/MissionDSA/Recursion/PrnitIncOrder.java
@@ -0,0 +1,19 @@
+package MissionDSA.Recursion;
+
+public class PrnitIncOrder {
+
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printInc(n);
+    }
+}

c0553736c31d661516e0b3166a8da30189971cb5
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-11-04 17:30:45 +0530

    Print in Decreasing order

--- src/MissionDSA/Recursion/PrintDecOrder.java
+++ src/MissionDSA/Recursion/PrintDecOrder.java
@@ -3,15 +3,15 @@ package MissionDSA.Recursion;
 // to smaller the instances of the same problem.
 
 //Problem 1 -> Print numbers n to 1 (Decreasing order) using recursion
-public class Intro {
+public class PrintDecOrder {
 
     public static void printN(int n){
-        if(n ==1){          //This condition ios the base case
+        if(n ==1){          //This condition is the base case
             System.out.println(n);
             return;
         }
-        System.out.print(n+" ");
-        printN(n-1);
+        System.out.print(n+" ");  // For example -> 10
+        printN(n-1); // These are the numbers 9 to 1.
     }
     public static void main(String[] args) {
         int n = 10;

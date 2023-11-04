c3dc37fa11b46d533ddc3ded0703936c499d8c32
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2023-11-04 17:37:10 +0530

    Print in Decreasing order

--- src/MissionDSA/Recursion/PrnitIncOrder.java
+++ src/MissionDSA/Recursion/PrnitIncOrder.java
@@ -3,13 +3,13 @@ package MissionDSA.Recursion;
 public class PrnitIncOrder {
 
     public static void printInc(int n){
+        // Base Case
         if(n==1){
             System.out.print(n+" ");
             return;
         }
-        printInc(n-1);
-        System.out.print(n+" ");
-
+        printInc(n-1);          // First print number 1 to 9
+        System.out.print(n+" ");   // Then print n i.e. 10
     }
 
     public static void main(String[] args) {
